7172bd13fe8dcade77e9fd2ceacab0e4e3c7fda6
Patricio Ferraggi <pattferraggi@gmail.com>
2018-05-17 23:20:27 +0200

    added ngfor for repeteable stuff

--- src/app/components/views/home/home.component.ts
+++ src/app/components/views/home/home.component.ts
@@ -5,11 +5,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  boxes = [
+    {
+      link: "assets/img/logo_html.png",
+      title: "Html5 Markup",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia mauris id dui finibus dignissim."
+    },
+    {
+      link: "assets/img/logo_css.png",
+      title: "CSS3",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia mauris id dui finibus dignissim."
+    },
+    {
+      link: "assets/img/logo_brush.png",
+      title: "Graphic Design",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia mauris id dui finibus dignissim."
+    }
+  ];
 
 }
--- src/app/components/views/services/services.component.ts
+++ src/app/components/views/services/services.component.ts
@@ -12,4 +12,22 @@ export class ServicesComponent implements OnInit {
   ngOnInit() {
   }
 
+  services = [
+    {
+      title: "Website Design",
+      text: "Aenean quis velit urna. Proin in tortor dictum, congue eros id, rhoncus libero. Proin bibendum elit erat, a eleifend",
+      pricing: "$1000 - $3000"
+    },
+    {
+      title: "Website Manteinance",
+      text: "Aenean quis velit urna. Proin in tortor dictum, congue eros id, rhoncus libero. Proin bibendum elit erat, a eleifend",
+      pricing: "$1000 - $3000"
+    },
+    {
+      title: "Website Host",
+      text: "Aenean quis velit urna. Proin in tortor dictum, congue eros id, rhoncus libero. Proin bibendum elit erat, a eleifend",
+      pricing: "$1000 - $3000"
+    }
+  ];
+
 }
